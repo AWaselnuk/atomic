de1c5c94b87a4aa3cf84eb8a85ea4f0da4cc3d5f
Adam Waselnuk <awaselnuk@mygrocerydeals.com>
2014-12-15 12:09:30 -0500

    Specs for options.contentType

--- test/atomic-spec.js
+++ test/atomic-spec.js
@@ -10,6 +10,7 @@ describe('atomic', function () {
 
     beforeEach(function () {
       spyOn(XMLHttpRequest.prototype, 'open').andCallThrough();
+      spyOn(XMLHttpRequest.prototype, 'setRequestHeader').andCallThrough();
       spyOn(XMLHttpRequest.prototype, 'send');
     });
 
@@ -18,9 +19,39 @@ describe('atomic', function () {
       .success(function (data, xhr) {
       })
       .error(function (data, xhr) {
-
       });
       expect(XMLHttpRequest.prototype.open).toHaveBeenCalled();
+    });
+
+    it('should send an XMLHttpRequest', function () {
+      atomic.get('')
+      .success(function (data, xhr) {
+      })
+      .error(function (data, xhr) {
+      });
+      expect(XMLHttpRequest.prototype.send).toHaveBeenCalled();
+    });
+
+    it('should set Content-Type header to "application/x-www-form-urlencoded"', function () {
+      atomic.get('')
+      .success(function (data, xhr) {
+      })
+      .error(function (data, xhr) {
+      });
+      expect(XMLHttpRequest.prototype.setRequestHeader).toHaveBeenCalledWith('Content-type', 'application/x-www-form-urlencoded');
+    });
+
+    it('accepts an options param that can change the Content-Type header', function () {
+      var options = {
+        contentType: 'application/json'
+      };
+
+      atomic.get('', options)
+      .success(function (data, xhr) {
+      })
+      .error(function (data, xhr) {
+      });
+      expect(XMLHttpRequest.prototype.setRequestHeader).toHaveBeenCalledWith('Content-type', 'application/json');
     });
 
   });
